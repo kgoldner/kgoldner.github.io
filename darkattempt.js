fbe23ca0481d597c3762bbe02f272bd277403524
Kira Goldner <kgoldner@cs.washington.edu>
2020-04-27 22:39:56 -0400

    to single link

--- darkattempt.js
+++ darkattempt.js
@@ -1,3 +1,4 @@
+/*
 function swapStyleSheet(sheet){
   document.getElementById('theme').setAttribute('href', sheet);
   localStorage.setItem("sheet", sheet);
@@ -7,3 +8,19 @@ window.onload = _ =>
  swapStyleSheet(
   localStorage.getItem("sheet") || "default.css"
  );
+ */
+ 
+function swapStyleSheet(){
+    var href = document.getElementById('theme').getAttribute('href');
+    var new_href = 'updated.css';
+
+    if (href == 'updated.css') {
+        new_href = 'darkmode.css';
+    }
+
+    document.getElementById('theme').setAttribute('href', new_href);
+        localStorage.setItem("sheet", new_href);
+    }
+
+window.onload = _ =>
+    swapStyleSheet();
