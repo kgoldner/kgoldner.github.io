113390ae9a73fbad3a9f27e8015de83de918db17
Kira Goldner <kgoldner@cs.washington.edu>
2020-05-01 13:08:43 -0400

    first multipage site attempt

---
+++ multipagesite/toggledark.js
@@ -0,0 +1,26 @@
+/*
+function swapStyleSheet(sheet){
+  document.getElementById('theme').setAttribute('href', sheet);
+  localStorage.setItem("sheet", sheet);
+}
+
+window.onload = _ =>
+ swapStyleSheet(
+  localStorage.getItem("sheet") || "default.css"
+ );
+ */
+ 
+function swapStyleSheet(){
+    var href = document.getElementById('theme').getAttribute('href');
+    var new_href = 'update.css';
+
+    if (href == 'update.css') {
+        new_href = 'dark.css';
+    }
+
+    document.getElementById('theme').setAttribute('href', new_href);
+        localStorage.setItem("sheet", new_href);
+    }
+
+window.onload = _ =>
+    swapStyleSheet();
