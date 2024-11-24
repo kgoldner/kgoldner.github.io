af1d0f86437c2b61ff8c69e9af718001642203a5
Kira Goldner <kgoldner@cs.washington.edu>
2024-11-24 14:52:45 -0500

    fix bold

--- navigation.js
+++ navigation.js
@@ -22,7 +22,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
             // Highlight the current section
             const pathParts = window.location.pathname.split('/').filter(Boolean);
-            const currentPage = pathParts.length > 0 ? pathParts[pathParts.length - 1] : '';
+            const currentPage = pathParts.length > 0 ? pathParts[1] : '';
             
             // Remove ".html" extension from the current page name
             const currentSection = currentPage.replace('.html', '');
